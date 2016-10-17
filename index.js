e0a70609750e6d24ae32de8dcf524f899eee8a8f
peelingtheonion <xxxfutures@gmail.com>
2016-10-17 13:17:40 +0800

    wechat 页面

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express')(),
     //引入配置文件
     conf = require('./lib/config'),
+    //引入系统提供的 path 包
+    path = require('path'),
     //引入微信 API 功能包
     wechat = require('./lib/wechat'),
     bodyParser = require('body-parser');
@@ -10,6 +12,23 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+//设置模板目录
+app.set('views', path.join(__dirname,'lib/views'));
+//设置模板引擎
+app.set('views engine', 'ejs');
+
+//静态文件目录，在 Express 中提供如图像,CSS 文件和 JavaScript 文件之类的静态文件时，使用 Express 中的 express.static 内置中间件函数。
+app.use(
+    express.static(
+        path.join(__dirname, 'static'),
+        {
+            index: false,
+            maxAge: '7 days'
+
+        }
+    )
+);
+
 //初始化微信 API 功能包
 wechat(conf.wechat);
 
@@ -18,6 +37,7 @@ wechat(conf.wechat);
 wechat.createMenu(require('./lib/menu.json'));
 
 app.use('/wxapi',require('./lib/routers/wxapi.js'));
+app.use('/pages',require('./lib/routers/pages.js'));
 
 app.listen(8005,function(err){
     console.log('listening at 8005...')
